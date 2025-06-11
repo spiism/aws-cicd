b6a31fc0fcdf1ce6eb6866a85f0997166c920ab7
michael <spiism@gmail.com>
2025-06-11 23:49:31 +1200

    update main.py

--- lambda/main.py
+++ lambda/main.py
@@ -2,12 +2,54 @@ import json
 import os
 
 def handler(event, context):
-    version = os.environ.get("VERSION", "0.0")
-    respond_body = {
-        "message": "test 2",
-        "version": version 
+    
+    profile_data = {
+        "name": "Michael Ho", 
+        "job": "Full-Stack Software Engineer",
     }
-    return {
-        "statusCode": 200, 
-        "body": json.dumps(respond_body) 
-    }
+    
+    try:
+        # Get HTTP method
+        http_method = event.get('httpMethod', event.get('requestContext', {}).get('http', {}).get('method', 'GET'))
+        
+        if http_method == 'GET':
+            response_body = {
+                "success": True,
+                "data": profile_data,
+                "version": os.environ.get("VERSION", "1.0.0"),
+                "note": "This is a simple version without database integration"
+            }
+            status_code = 200
+        else:
+            response_body = {
+                "success": False,
+                "message": f"Method {http_method} not allowed",
+                "version": os.environ.get("VERSION", "1.0.0")
+            }
+            status_code = 405
+            
+        return {
+            "statusCode": status_code,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type"
+            },
+            "body": json.dumps(response_body, default=str)
+        }
+        
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps({
+                "success": False,
+                "message": "Internal server error",
+                "error": str(e),
+                "version": os.environ.get("VERSION", "1.0.0")
+            })
+        }
